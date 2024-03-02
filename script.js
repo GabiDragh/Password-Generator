6aa8621a875caddaaa429ba9bc95c19d412c611f
Gabi Draghici <draghici.gabi89@gmail.com>
2024-03-02 00:49:27 +0000

    App generates password, but prompts multiple times for char types. Needs that fixing

--- script.js
+++ script.js
@@ -88,39 +88,44 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-var passLength = Number(prompt("Please select your password length between 8 and 128 characters:")); //variable moved from inside function to a global position because it is used again in 
+var passLength;
+var charTypes = [];
 
 // 1. Function to prompt user for password options
 function getPasswordOptions() {
+
+  passLength = Number(prompt("Please select your password length between 8 and 128 characters:")); //variable moved from inside function to a global position because it is used again in 
 
  //1.1 Prompt user for password length (8 to 128 characters)
  //var passLength = Number(prompt("Please select your password length between 8 and 128 characters:")); 
                                                                                                                     
  //1.2 If password length not correct, prompt `Password not correct length and return to select password length prompt"
-  while (passLength < 8 || passLength > 128) {
+  while (passLength < 8 || passLength > 128 || isNaN(passLength)) {
    alert("Please choose a number between 8 and 128:");
    passLength = Number(prompt("Please select your password length between 8 and 128 characters:")); 
    }
 
  //1.3 Ask user to select a character type from specialCharacters, numericCharacters, lowerCasedCharacters, upperCasedCharacters. 
- var charTypes = [{
+ charTypes.push ({
   name: "special characters",
   type: specialCharacters,
   user_selected: true
- }, 
+ }); 
+ charTypes.push (
   {name: "numeric characters",
   type: numericCharacters,
   user_selected: true
- }, 
-
+ }); 
+ charTypes.push (
   {name: "lower case characters",
   type: lowerCasedCharacters,
   user_selected: true
- }, 
+ });
+ charTypes.push ( 
   {name: "upper case characters",
   type: upperCasedCharacters,
   user_selected: true
- }];
+ });
 
  for (var i=0; i < charTypes.length; i++) {
   charTypes[i].user_selected = confirm("Should the password include " + charTypes[i].name + " ?");
@@ -128,7 +133,7 @@ function getPasswordOptions() {
 
  //1.4 If user does not select any, prompt 'Please select at least one character type' and return to selection. If user selects at least one character type, then proceed to next step.
 
- for (var i=0; i < charTypes.length; i++) {
+ for (var i = 0; i < charTypes.length; i++) {
   var selectConfirm = charTypes[i].user_selected;
 
     if (selectConfirm === true) {
@@ -144,31 +149,25 @@ function getPasswordOptions() {
 
 // 2. Function for getting a random element from an array
 function getRandom(charArr) {
-
- //2.1 Created an array variable combining the special characters arrays to randomly select from
- var charArr = [specialCharacters, numericCharacters, lowerCasedCharacters, upperCasedCharacters];
- console.log(charArr);
-
- //2.2 Random character selection
- var selectedArr = charArr[Math.floor(Math.random() * charArr.length)];
- return selectedArr[Math.floor(Math.random() * charArr.length)];
+ return charArr[Math.floor(Math.random() * charArr.length)];
+}
 
 // 3. Function to generate password with user input
  function generatePassword() {
-  //3.1 Defined password generated criteria
-  var { passLength, selectedArr} = getPasswordCriteria;
-  //3.2 Defined password variable
+
+  getPasswordOptions();
+  var selectedArr = [];
   var password = ""; 
   
-  for(let i = 0; i < passLength; i++) {
-    password += passLength-1 + selectedArr;
+  for ( var i = 0; i < passLength; i++) {
+    var randomIndex = Math.floor(Math.random() * charTypes.length);
+    selectedArr = selectedArr.concat(charTypes[randomIndex].type);
+    password += getRandom(selectedArr);
   }
   
-  console.log(password);
+  return password;
 }
-}
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+
 
 // Write password to the #password input
 function writePassword() {
@@ -179,14 +178,14 @@ function writePassword() {
 }
 
 // 6. Add event listener to generate button
-var generateButton = document.getElementById('generateBtn');
+var generateBtn = document.querySelector('#generate');
 generateBtn.addEventListener('click', writePassword);
 
-//7. Call all functions used in this file
+// //7. Call all functions used in this file
 
-getPasswordOptions(); 
-getRandom(specialCharacters);
-getRandom(numericCharacters);
-getRandom(lowerCasedCharacters);
-getRandom(upperCasedCharacters);
-generatePassword();
+// getPasswordOptions(); 
+// getRandom(specialCharacters);
+// getRandom(numericCharacters);
+// getRandom(lowerCasedCharacters);
+// getRandom(upperCasedCharacters);
+// writePassword();
